63474bfc2986d71985b1fb9f23885bd845ab8cc1
Gregory Wood <greg@wood.name>
2015-10-02 06:17:26 -0400

    Add CORS header

--- AlertGo.go
+++ AlertGo.go
@@ -90,7 +90,9 @@ func GetAlerts(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("error:", err)
 	}
+
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(j)
 }
 
